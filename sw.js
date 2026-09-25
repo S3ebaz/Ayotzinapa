ae1891be2660d90379e5e0e5e229c314ae8cb4bb
S3ebaz <pazcervantessebastian@gmail.com>
2026-09-24 20:16:07 -0600

    Cache ligero para repetir la visita sin gastar datos

--- sw.js
+++ sw.js
@@ -1,11 +1,26 @@
-self.addEventListener('install', function(e){ self.skipWaiting(); });
+const CACHE='ayo-movil-v2';
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(['./','./index.html','./app.js']); }).then(function(){ return self.skipWaiting(); }));
+});
 self.addEventListener('activate', function(e){
-  e.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
-    }).then(function(){ return self.clients.claim(); })
-  );
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
 });
 self.addEventListener('fetch', function(e){
-  e.respondWith(fetch(e.request, {cache:'no-store'}).catch(function(){ return caches.match(e.request); }));
+  var url=e.request.url;
+  if(e.request.method!=='GET') return;
+  if(url.indexOf('upload.wikimedia.org')!==-1 || url.indexOf('centrodemedioslibres.org')!==-1){
+    e.respondWith(fetch(e.request).catch(function(){ return caches.match(e.request); }));
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      if(res.ok && url.indexOf(self.location.origin)===0){
+        var copy=res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+      }
+      return res;
+    }).catch(function(){ return caches.match(e.request); })
+  );
 });
